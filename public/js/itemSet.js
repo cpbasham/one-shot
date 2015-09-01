edd03bbfdb05703d63cd6b48584a9bca2d05e759
Cameron Basham <cbasham@gm.slc.edu>
2015-08-31 18:53:47 -0700

    make things addable and deletable

--- public/js/itemSet.js
+++ public/js/itemSet.js
@@ -15,14 +15,17 @@ $("div.item-set-container").on("click", "div.new-row", function(evt) {
     var newRow = $(data).insertBefore(target);
     newRow.find("ul").droppable({
       drop: function( e, ui ) {
-
-        var li = $('<li><span data-type="item" data-id="' + ui.draggable.data("id") + '" title="' + ui.draggable.attr("title") + '" style="' + ui.draggable.attr("style") + '" class="sprite"</li>');
-        // console.log(li);
+        e.preventDefault();
+        e.stopPropagation();
+        var li = $('<li><span data-type="item" data-id="' + ui.draggable.data("id") + '" title="' + ui.draggable.attr("title") + '" style="' + ui.draggable.attr("style") + '" class="sprite"></span></li>');
         li.appendTo(e.target);
-        // ui.helper.parent().clone().appendTo(e.target);
       }
     });
   });
+});
+
+$("div.item-set-container").on("dblclick", "span.sprite", function(e) {
+  $(e.target).parent().remove();
 });
 
 $("div.item-set-container").on("click", "div.item-set-row-container div.row-items", function(e) {
@@ -31,11 +34,6 @@ $("div.item-set-container").on("click", "div.item-set-row-container div.row-item
   container.siblings().find("div.item-set-row-delete").hide();
 });
 
-// $("div.item-set").on("click", "div.item-set-row-container", function(e) {
-//   $(e.target).closest("div.item-set-row-container").find("div.item-set-row-delete").show();
-//   $(e.target).closest("div.item-set-row-container").siblings().find("div.item-set-row-delete").hide();
-// });
-
-// $("body").on("click", ":not(div.item-set-row-container)", function(e) {
-//   $("div.item-set-row-container div.item-set-row-delete").hide();
-// });
+$("div.item-set-container").on("dblclick", "div.item-set-row-delete", function(e) {
+  $(e.target).parent().remove();
+});
